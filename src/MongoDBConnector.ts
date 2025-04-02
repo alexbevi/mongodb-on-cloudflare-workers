5134921218de8a1898b30ab978ab2da7bc49483f
Alex Bevilacqua <alex@alexbevi.com>
2025-04-02 10:34:13 -0400

    update

---
+++ src/MongoDBConnector.ts
@@ -0,0 +1,44 @@
+import { MongoClient } from 'mongodb';
+
+interface Env {
+  MONGODB_URI: string;
+}
+
+export class MongoDBDConnector {
+  private env: Env;
+  private client: MongoClient;
+
+  constructor(env: Env) {
+    this.env = env;
+    this.client = new MongoClient(this.env.MONGODB_URI);
+  }
+
+  async getMovie() {
+    try {
+      const queryStartTime = Date.now();
+
+      await this.client.connect();
+      const db = this.client.db("sample_mflix");
+      const movies = db.collection("movies");
+
+      const movie = await movies.findOne({ title: 'Back to the Future' });
+      const queryTime = Date.now() - queryStartTime;
+
+      if (!movie) {
+        throw new Error('Movie not found');
+      }
+
+      return {
+        movie: {
+          ...movie,
+          _id: movie._id.toString(),
+        },
+        queryTime
+      };
+
+    } catch (error) {
+      console.error('MongoDB error:', error);
+      throw error;
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
-import { MongoClient } from 'mongodb';
-
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+import { MongoDBConnector } from './MongoDBConnector';
+// import { MongoDBDurableConnector } from './MongoDBDurableConnector';
 interface Env {
   MONGODB_URI: string;
+  // MY_DURABLE_OBJECT: DurableObjectNamespace<MongoDBConnector>;
 }
 
 export default {
@@ -22,46 +10,18 @@ export default {
     const { searchParams } = new URL(request.url)
     let durable = searchParams.get('durable')
 
+    // const id = env.MY_DURABLE_OBJECT.idFromName("mongodb-connector");
+    // const stub = env.MY_DURABLE_OBJECT.get(id);
 
-    const client = new MongoClient(env.MONGODB_URI);
-    const events = ["topologyOpening", "topologyClosed","connectionCreated","connectionReady","connectionPoolCreated"];
-    const messages: string[] = [];
-    for(var i = 0; i < events.length; i++) {
-      client.on(events[i], (event) => {
-        messages.push(`${Date.now()} | ${JSON.stringify(event)}`);
-      });
-    }
-
-    try {
-      const queryStartTime = Date.now();
-
-			await client.connect();
-
-      const db = client.db("sample_mflix");
-      const movies = db.collection("movies");
-
-      // Find the movie
-      const movie = await movies.findOne({ title: 'Back to the Future' });
-
-			const queryTime = Date.now() - queryStartTime;
-
-      if (!movie) {
-        return new Response('Movie not found', { status: 404 });
-      }
-
-      return Response.json({
-				messages,
-        movie,
-				queryTime,
-        durable
-			});
-
-    } catch (error) {
-      console.error('MongoDB error:', error);
-      const message = error instanceof Error ? error.message : 'Unknown error';
-      return new Response(`Error: ${message}`, { status: 500 });
-    } finally {
-      await client.close();
-    }
+    const proxy = new MongoDBConnector(env)
+    const result = await proxy.getMovie();
+    return Response.json(result);
+  } catch (error) {
+    console.error('Error:', error);
+    const message = error instanceof Error ? error.message : 'Unknown error';
+    return new Response(`Error: ${message}`, { status: 500 });
   }
+}
 } satisfies ExportedHandler<Env>;
+
+export { MongoDBConnector };
