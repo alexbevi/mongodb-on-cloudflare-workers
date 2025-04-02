0e6a6f1374fcdfcd5816917a28491dd49bdfd9e8
Alex Bevilacqua <alex@alexbevi.com>
2025-04-02 10:37:11 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -9,19 +9,19 @@ export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const { searchParams } = new URL(request.url)
     let durable = searchParams.get('durable')
+    try {
+      // const id = env.MY_DURABLE_OBJECT.idFromName("mongodb-connector");
+      // const stub = env.MY_DURABLE_OBJECT.get(id);
 
-    // const id = env.MY_DURABLE_OBJECT.idFromName("mongodb-connector");
-    // const stub = env.MY_DURABLE_OBJECT.get(id);
-
-    const proxy = new MongoDBConnector(env)
-    const result = await proxy.getMovie();
-    return Response.json(result);
-  } catch (error) {
-    console.error('Error:', error);
-    const message = error instanceof Error ? error.message : 'Unknown error';
-    return new Response(`Error: ${message}`, { status: 500 });
+      const proxy = new MongoDBConnector(env)
+      const result = await proxy.getMovie();
+      return Response.json(result);
+    } catch (error) {
+      console.error('Error:', error);
+      const message = error instanceof Error ? error.message : 'Unknown error';
+      return new Response(`Error: ${message}`, { status: 500 });
+    }
   }
-}
 } satisfies ExportedHandler<Env>;
 
 export { MongoDBConnector };
