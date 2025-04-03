086aa6dab371e30a0176b1b13514aee093bc0563
Alex Bevilacqua <alex@alexbevi.com>
2025-04-03 15:50:39 -0400

    update

--- src/MongoDBDurableConnector.ts
+++ src/MongoDBDurableConnector.ts
@@ -19,24 +19,20 @@ export class MongoDBDurableConnector extends DurableObject {
     try {
       const queryStartTime = Date.now();
       await this.client.connect();
-      // Get the database and collection on which to run the operation
-      const database = client.db("sample_mflix");
+
+      const database = this.client.db("sample_mflix");
       const movies = database.collection("movies");
-      // Query for a movie that has the title 'The Room'
       const query = { title: "The Room" };
       const options = {
-        // Sort matched documents in descending order by rating
         sort: { "imdb.rating": -1 },
-        // Include only the `title` and `imdb` fields in the returned document
-        projection: { _id: 0, title: 1, imdb: 1 },
+        projection: { title: 1, imdb: 1 },
       };
-      // Execute query
       const movie = await movies.findOne(query, options);
+
       const queryTime = Date.now() - queryStartTime;
       return {
         movie: {
-          ...movie,
-          _id: movie._id.toString(),
+          ...movie
         },
         queryTime
       };
